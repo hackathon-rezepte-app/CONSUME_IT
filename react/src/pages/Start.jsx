b21e6e73603b583dbd98dfbc49943fa32c6b6a46
Peter Hagen <phgn@protonmail.ch>
2019-04-12 22:16:18 +0200

    Integrate screen1 design

--- react/src/pages/Start.jsx
+++ react/src/pages/Start.jsx
@@ -1,25 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { withRouter } from "react-router";
 
 function Start({ queryItems, history }) {
-  const [picture, setPicture] = useState(null);
   return (
-    <div>
-      Start
-      <input
-        className="file-input"
-        type="file"
-        accept="image/*"
-        onChange={event => setPicture(event.target.files[0])}
-      />
-      <button
-        onClick={() => {
-          history.push("/correct");
-          queryItems(picture);
-        }}
-      >
-        Weiter
-      </button>
+    <div className="container">
+      <div className="row">
+        <div className="one-half column" style={{ marginTop: "25%" }}>
+          <h4>TAKE A PICTURE</h4>
+          <h5>
+            <small>and save some time for more netflix</small>
+          </h5>
+          <form>
+            <label for="camera-upload" className="label-upload" />
+            <br />
+            <input
+              id="camera-upload"
+              className="file-input"
+              type="file"
+              accept="image/*"
+              onChange={event => {
+                queryItems(event.target.files[0]);
+                history.push("/correct");
+              }}
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
