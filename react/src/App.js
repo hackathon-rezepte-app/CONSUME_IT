c7bd6232ee8c9b592fd2e1e9374fca4485e2c8bf
Peter Hagen <phgn@protonmail.ch>
2019-04-13 00:10:57 +0200

    Fix css issues

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Axios from "axios";
-import "./App.css";
 import Start from "./pages/Start";
 import Correct from "./pages/Correct";
 import Recipes from "./pages/Recipes";
