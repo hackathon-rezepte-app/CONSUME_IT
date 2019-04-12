ff302cd59b1b4dc9bbacb8c66636954a9dc9e138
Peter Hagen <phgn@protonmail.ch>
2019-04-12 16:46:51 +0200

    Add basic page structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Start from "./pages/Recipes";
+import Correct from "./pages/Correct";
+import Recipes from "./pages/Recipes";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Route path="/" exact component={Start} />
+      <Route path="/correct/" component={Correct} />
+      <Route path="/recipes/" component={Recipes} />
+    </Router>
+  );
 }
-
-export default App;
