e51ebcb2175a312336f9f59cbd94da2aecdcbbb4
Peter Hagen <phgn@protonmail.ch>
2019-04-12 23:57:34 +0200

    Integrate new html/css screen

--- react/src/pages/Correct.jsx
+++ react/src/pages/Correct.jsx
@@ -3,9 +3,37 @@ import { withRouter } from "react-router";
 
 function Correct({ items, setItems, queryRecipes, history }) {
   return (
-    <div>
-      Erkannte Lebensmittel:
-      <ul>{items && items.map(item => <ul>{item.name}</ul>)}</ul>
+    <div class="container">
+      <div class="row">
+        <div class="one-half column" style="margin-top: 25%">
+          <h4>WE FOUND:</h4>
+          <h5>
+            <small>please correct, robots are only robots</small>
+          </h5>
+
+          <ul>
+            {items &&
+              items.map(item => (
+                <li class="item-list">
+                  item.name
+                  <div class="item-list-control">
+                    <label
+                      for="button-ok"
+                      class="button-ok-label button-label-screen3"
+                    />
+                    <input id="button-ok" type="submit" class="button-ok" />
+
+                    <label
+                      for="button-no"
+                      class="button-no-label button-label-screen3"
+                    />
+                    <input id="button-no" class="button-no" type="submit" />
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
       <button
         onClick={() => {
           history.push("/recipes");
