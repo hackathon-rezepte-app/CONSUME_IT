5fc721a314ee67a37709e263df39df0f836b9da5
Peter Hagen <phgn@protonmail.ch>
2019-04-13 11:20:41 +0200

    Add refresh button

--- react/src/pages/Correct.jsx
+++ react/src/pages/Correct.jsx
@@ -4,76 +4,94 @@ import { ClipLoader } from "react-spinners";
 
 function Correct({ items, setItems, queryRecipes, history }) {
   function deleteItem(index) {
+    console.log(index);
     items.splice(index, 1);
     setItems([...items]);
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="one-half column" style={{ marginTop: "25%" }}>
-          <h4>WE FOUND:</h4>
-          <h5>
-            <small>please correct, robots are only robots</small>
-          </h5>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column" style={{ marginTop: "25%" }}>
+            <h4>WE FOUND:</h4>
+            <h5>
+              <small>please correct, robots are only robots</small>
+            </h5>
 
-          {!items && (
-            <ClipLoader
-              css={{ marginTop: "20px" }}
-              sizeUnit={"px"}
-              size={50}
-              color={"#7AC6B8"}
-              loading={true}
-            />
-          )}
+            {!items && (
+              <ClipLoader
+                css={{ marginTop: "20px" }}
+                sizeUnit={"px"}
+                size={50}
+                color={"#7AC6B8"}
+                loading={true}
+              />
+            )}
 
-          <ul>
-            {items &&
-              items.map((item, index) => (
-                <li key={index} className="item-list">
-                  {item.tagName}
-                  <div className="item-list-control">
-                    <label
-                      htmlFor="button-ok"
-                      className="button-ok-label button-label-screen3"
-                    />
-                    <input id="button-ok" type="submit" className="button-ok" />
+            <ul>
+              {items &&
+                items.map((item, index) => (
+                  <li key={index} className="item-list">
+                    {item.tagName}
+                    <div className="item-list-control">
+                      <label
+                        htmlFor="button-ok"
+                        className="button-ok-label button-label-screen3"
+                      />
+                      <input
+                        id="button-ok"
+                        type="submit"
+                        className="button-ok"
+                      />
 
-                    <label
-                      htmlFor="button-no"
-                      className="button-no-label button-label-screen3"
-                    />
-                    <input
-                      id="button-no"
-                      className="button-no"
-                      type="submit"
-                      onClick={() => deleteItem(index)}
-                    />
-                  </div>
-                </li>
-              ))}
-          </ul>
+                      <label
+                        htmlFor="button-no"
+                        className="button-no-label button-label-screen3"
+                      />
+                      <input
+                        id="button-no"
+                        className="button-no"
+                        type="submit"
+                        onClick={() => deleteItem(index)}
+                      />
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
+
+        {items && items.length > 0 && (
+          <>
+            <label
+              htmlFor="button-next"
+              className="button-next-label button-label-screen3"
+            />
+            <input
+              id="button-next"
+              type="submit"
+              className="button-next"
+              onClick={() => {
+                history.push("/recipes");
+                queryRecipes(items);
+              }}
+            />
+          </>
+        )}
       </div>
 
-      {items && items.length > 0 && (
-        <>
-          <label
-            htmlFor="button-next"
-            className="button-next-label button-label-screen3"
-          />
-          <input
-            id="button-next"
-            type="submit"
-            className="button-next"
-            onClick={() => {
-              history.push("/recipes");
-              queryRecipes(items);
-            }}
-          />
-        </>
-      )}
-    </div>
+      <label
+        htmlFor="button-refresh"
+        class="button-refresh-label button-label-screen3"
+      />
+      <input
+        id="button-refresh"
+        type="submit"
+        className="button-refresh"
+        onClick={() => history.push("/")}
+      />
+    </>
   );
 }
 
--- react/src/pages/Recipes.jsx
+++ react/src/pages/Recipes.jsx
@@ -4,50 +4,63 @@ import { ClipLoader } from "react-spinners";
 
 function Recipes({ recipes, history }) {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="one-half column" style={{ marginTop: "25%" }}>
-          <h4>MAYBE YOU COOK:</h4>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column" style={{ marginTop: "25%" }}>
+            <h4>MAYBE YOU COOK:</h4>
 
-          {!recipes && (
-            <ClipLoader
-              css={{ marginTop: "40px" }}
-              sizeUnit={"px"}
-              size={50}
-              color={"#7AC6B8"}
-              loading={true}
-            />
-          )}
+            {!recipes && (
+              <ClipLoader
+                css={{ marginTop: "40px" }}
+                sizeUnit={"px"}
+                size={50}
+                color={"#7AC6B8"}
+                loading={true}
+              />
+            )}
 
-          {recipes && (
-            <>
-              <h5>
-                <small>have a nice meal!</small>
-              </h5>
+            {recipes && (
+              <>
+                <h5>
+                  <small>have a nice meal!</small>
+                </h5>
 
-              <ul>
-                {recipes.map((recipe, index) => (
-                  <li key={index} className="item-list">
-                    {recipe.name}
-                    <div className="item-list-control">
-                      <label
-                        htmlFor="button-show-recipe"
-                        className="button-recipe-label button-label-screen3"
-                      />
-                      <input
-                        id="button-show-recipe"
-                        className="button-ok"
-                        onClick={() => history.push("/detail/" + index)}
-                      />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
+                <ul>
+                  {recipes.map((recipe, index) => (
+                    <li key={index} className="item-list">
+                      {recipe.name}
+                      <div className="item-list-control">
+                        <label
+                          htmlFor="button-show-recipe"
+                          className="button-recipe-label button-label-screen3"
+                        />
+                        <input
+                          id="button-show-recipe"
+                          className="button-ok"
+                          onClick={() => history.push("/detail/" + index)}
+                        />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <label
+        htmlFor="button-refresh"
+        class="button-refresh-label button-label-screen3"
+      />
+      <input
+        id="button-refresh"
+        type="submit"
+        className="button-refresh"
+        onClick={() => history.push("/")}
+      />
+    </>
   );
 }
 
